b4331c69d13995ba87092e255c1e179e79ecf64c
Rin-ui <rajashri.biology@gmail.com>
2025-01-15 16:40:36 +0530

    power.c

--- power.c
+++ power.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
   int a,b,ans ;
   printf("Enter a and b = ");
   scanf("%d%d",&a,&b);
-  ans = a**b ;
+  ans = pow(a,b);
   printf("Ans = %d",ans);
   return(0);
 }
